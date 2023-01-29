2e7805ded17c104f3fe217113a59e1593d6cc0a9
junsuPark <krpjs0508@gmail.com>
2023-01-29 22:52:28 +0900

    UserRepository

---
+++ jwt/user-service/src/main/kotlin/app/junsu/jwt/domain/repository/user/UserRepository.kt
@@ -0,0 +1,6 @@
+package app.junsu.jwt.domain.repository.user
+
+import app.junsu.jwt.domain.entity.user.User
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+
+interface UserRepository : CoroutineCrudRepository<User, Long>

4a2f3e67184d4eeca40475b120790b043513b8c3
junsuPark <krpjs0508@gmail.com>
2023-01-29 23:09:24 +0900

    findByEmail

--- jwt/user-service/src/main/kotlin/app/junsu/jwt/domain/repository/user/UserRepository.kt
+++ jwt/user-service/src/main/kotlin/app/junsu/jwt/domain/repository/user/UserRepository.kt
@@ -3,4 +3,7 @@ package app.junsu.jwt.domain.repository.user
 import app.junsu.jwt.domain.entity.user.User
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface UserRepository : CoroutineCrudRepository<User, Long>
+interface UserRepository : CoroutineCrudRepository<User, Long> {
+
+    suspend fun findByEmail(email: String) : User?
+}
